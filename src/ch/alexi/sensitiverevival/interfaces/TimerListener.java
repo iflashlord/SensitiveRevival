20f96cf77f16d2620c72833297b4677f3acc41c8
Alexander Schenkel <aschenkel@kadenpartner.ch>
2013-10-20 19:50:48 +0200

    cleanup

--- src/ch/alexi/sensitiverevival/interfaces/TimerListener.java
+++
@@ -1,9 +0,0 @@
-package ch.alexi.sensitiverevival.interfaces;
-
-import ch.alexi.sensitiverevival.events.GameTimerEvent;
-
-public interface TimerListener {
-	public void onTimerStarted(GameTimerEvent event);
-	public void onTimerTick(GameTimerEvent event);
-	public void onTimerPaused(GameTimerEvent event);
-}
